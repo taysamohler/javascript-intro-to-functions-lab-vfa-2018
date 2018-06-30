1b4cfdfd80d1d2fedb8affa3f24ebe384b3308db
Taysa Mohler <taysa.mohler@gmail.com>
2018-06-30 05:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   var lowercase = "hello"
   var uppercase = "HELLO"
-  if (string.toLowerCase() === lowercase){
+  if (string.toLowerCase() === string){
     return "I can't hear you!"
   }
-  if (string.toUpperCase() === uppercase){
+  if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
   if(string === "I love you, Grandma."){
