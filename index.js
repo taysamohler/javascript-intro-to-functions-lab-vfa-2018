29b6515861bcc4a2a9675c66f6e1e5e6b13c13c6
Taysa Mohler <taysa.mohler@gmail.com>
2018-06-30 05:24:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase === uppercase){
     return "YES INDEED"
   }
+  if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
